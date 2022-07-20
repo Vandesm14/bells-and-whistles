4f7b66b886a838c510c1251e38fd11f35f51d734
Vandesm14 <vandesm14@gmail.com>
2022-07-20 17:33:29 -0400

    simple state demo

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,59 @@
 import { render } from 'preact';
 import { useState, useEffect } from 'preact/hooks';
+import { parseJsonSourceFileConfigFileContent } from 'typescript';
 
 const world = {
-  state: {},
+  state: {
+    engine: {
+      states: {
+        start: {
+          min: 0,
+          speed: 0.1,
+        },
+        ignition: {
+          min: 1,
+          speed: 2,
+        },
+        rising: {
+          min: 8,
+          speed: 1,
+        },
+        idle: {
+          min: 10,
+          speed: 0,
+        },
+      },
+      current_state: '',
+      last_update: Date.now(),
+      N1: 0,
+      switches: {
+        master: true,
+      },
+    },
+  },
 };
 
-function power(state: State): State {
+function engine(state: State): State {
+  const diff = Date.now() - state.engine.last_update;
+  state.engine.last_update = Date.now();
+  if (state.engine.switches.master) {
+    const engineState = Object.entries(state.engine.states)
+      .filter(([name, val]) => val.min <= state.engine.N1)
+      .slice(-1)[0];
+    if (!engineState) throw new Error('Could not find engine state');
+
+    state.engine.N1 += (diff / 1000) * engineState[1].speed;
+    state.engine.current_state = engineState[0];
+  } else if (state.engine.N1 > 0) {
+    state.engine.N1 -= diff / 1000;
+  } else {
+    state.engine.N1 = 0;
+  }
+
   return state;
 }
 
-const systems: Reducer[] = [power];
+const systems: Reducer[] = [engine];
 
 type State = typeof world['state'];
 type Reducer = (state: State) => State;
@@ -21,18 +65,16 @@ const pipe =
 
 function tick(state: State): State {
   state = structuredClone(state);
-
   state = pipe(...systems)(state);
-  console.log(state);
-
   return state;
 }
 
-setInterval(() => tick(world.state), 0);
-tick(world.state);
-
 const App = () => {
-  return <h1>Hey</h1>;
+  const [state, setState] = useState<State>(world.state);
+
+  setInterval(() => setState(tick), 0);
+
+  return <pre>{JSON.stringify(state, null, 2)}</pre>;
 };
 
 render(<App />, document.body);
