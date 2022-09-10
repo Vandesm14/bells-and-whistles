874816090ea2e57d6e29ca87c91eb0f80e82f7b6
Shane Vandegrift <vandesm14@gmail.com>
2022-09-10 19:49:38 -0400

    redo history lib

--- src/__tests__/history.test.ts
+++ src/__tests__/history.test.ts
@@ -1,120 +1,239 @@
-import { backward, forward, History, rollback } from '../lib/history';
+import {
+  generate,
+  push,
+  toIndex,
+  forward,
+  backward,
+  assemble,
+} from '../lib/history';
+
+const base = {
+  a: 1,
+  b: 2,
+  c: {
+    d: 3,
+    e: {
+      f: 4,
+    },
+  },
+};
 
 describe('history', () => {
-  it('should be able to forward', () => {
-    const history: History<{ a: number }> = [{ a: 1 }];
-    const newHistory = forward(history, { a: 2 });
-    expect(newHistory).toEqual([{ a: 1 }, { a: 2 }]);
-  });
+  it('should create a history object', () => {
+    const history = generate(base);
 
-  it('should decrease history when going backward', () => {
-    const history: History<{ a: number }> = [{ a: 1 }, { a: 2 }];
-    const newHistory = backward(history);
-    expect(newHistory).toEqual([{ a: 1 }]);
-  });
-
-  it('should not decrease history when going backward at the beginning', () => {
-    const history: History<{ a: number }> = [{ a: 1 }];
-    const newHistory = backward(history);
-    expect(newHistory).toEqual([{ a: 1 }]);
-  });
-
-  it('should keep a partial of each transaction', () => {
-    const initial = { a: 1, b: 2, c: 3 };
-    const history: History<typeof initial> = [initial];
-    const newHistory = forward(history, { a: 1, b: 2, c: 4 });
-
-    expect(newHistory).toEqual([{ a: 1, b: 2, c: 3 }, { c: 4 }]);
-  });
-
-  it('should rollback', () => {
-    const initial = {
-      a: 1,
-      b: 2,
-    };
-    let history: History<typeof initial> = [initial];
-
-    history = forward(history, {
-      a: 1,
-      b: 3, // changed
-    });
-
-    history = forward(history, {
-      a: 2, // changed
-      b: 3,
-    });
-
-    expect(rollback(history)).toEqual({ a: 1, b: 3 });
-  });
-
-  it('should rollback deep and apply partials recursively', () => {
-    const initial = {
-      a: 1,
-      b: {
-        c: 2,
-        d: 3,
-      },
-    };
-    let history: History<typeof initial> = [initial];
-
-    history = forward(history, {
-      a: 1,
-      b: {
-        c: 2,
-        d: 4, // changed
-      },
-    });
-
-    history = forward(history, {
-      a: 2, // changed
-      b: {
-        c: 2,
-        d: 4,
-      },
-    });
-
-    expect(rollback(history)).toEqual({
-      a: 1,
-      b: {
-        c: 2,
-        d: 4,
-      },
+    expect(history).toEqual({
+      list: [],
+      base,
+      value: base,
+      index: 0,
     });
   });
 
-  it('random history rollback', () => {
-    const randomOrSame = (value: number) =>
-      Math.random() > 0.5 ? value : Math.random() * 100;
+  describe('push', () => {
+    it('should push a new state to the history', () => {
+      let history = generate(base);
+      const newState = {
+        ...base,
+        a: 2,
+      };
 
-    const initial = {
-      a: 1,
-      b: 2,
-      c: {
-        d: 4,
-        e: 5,
-      },
-    };
+      history = push(history, newState);
 
-    let history: History<typeof initial> = [initial];
-    const index = Math.floor(Math.random() * 98) + 2;
-
-    let snapshot: typeof initial = initial;
-
-    for (let i = 0; i < 100; i++) {
-      const current = rollback(history);
-      if (i === index) {
-        snapshot = current;
-      }
-      history = forward(history, {
-        a: randomOrSame(current.a),
-        b: randomOrSame(current.b),
-        c: {
-          d: randomOrSame(current.c.d),
-          e: randomOrSame(current.c.e),
-        },
+      expect(history).toEqual({
+        list: [{ a: 2 }],
+        base,
+        value: newState,
+        index: 1,
       });
-    }
+    });
 
-    expect(rollback(history, index)).toEqual(snapshot);
+    it('should push a new deep state to the history', () => {
+      let history = generate(base);
+      const newState = {
+        ...base,
+        c: {
+          ...base.c,
+          e: {
+            ...base.c.e,
+            f: 5,
+          },
+        },
+      };
+
+      history = push(history, newState);
+
+      expect(history).toEqual({
+        list: [{ c: { e: { f: 5 } } }],
+        base,
+        value: newState,
+        index: 1,
+      });
+    });
+  });
+
+  describe('assemble', () => {
+    it('should assemble the history', () => {
+      let history = generate(base);
+      const newState = {
+        ...base,
+        a: 2,
+      };
+
+      history = push(history, newState);
+
+      const assembled = assemble(history);
+
+      expect(assembled).toEqual(newState);
+      expect(history.value).toEqual(newState);
+    });
+
+    it('should assemble the history with multiple states', () => {
+      let history = generate(base);
+
+      history = push(history, { ...base, a: 2 });
+      history = push(history, { ...base, a: 3 });
+      history = push(history, { ...base, a: 4 });
+
+      const assembled = assemble(history);
+
+      expect(assembled).toEqual({ ...base, a: 4 });
+      expect(history.value).toEqual({ ...base, a: 4 });
+    });
+  });
+
+  describe('toIndex', () => {
+    it('should treat 0 as the base value', () => {
+      let history = generate(base);
+      const newState = {
+        ...base,
+        a: 2,
+      };
+
+      history = push(history, newState);
+      history = toIndex(history, 0);
+
+      expect(history).toEqual({
+        list: [{ a: 2 }],
+        base,
+        value: base,
+        index: 0,
+      });
+    });
+
+    it('should clamp the index to the list length', () => {
+      let history = generate(base);
+      const newState = {
+        ...base,
+        a: 2,
+      };
+
+      history = push(history, newState);
+      history = toIndex(history, 2);
+
+      expect(history).toEqual({
+        list: [{ a: 2 }],
+        base,
+        value: newState,
+        index: 1,
+      });
+    });
+
+    it('should assemble the history to the index', () => {
+      let history = generate(base);
+
+      history = push(history, { ...base, a: 2 });
+      history = push(history, { ...base, a: 3 });
+      history = push(history, { ...base, a: 4 });
+
+      history = toIndex(history, 1);
+      expect(history).toEqual({
+        list: [{ a: 2 }, { a: 3 }, { a: 4 }],
+        base,
+        value: { ...base, a: 2 },
+        index: 1,
+      });
+
+      history = toIndex(history, 2);
+      expect(history).toEqual({
+        list: [{ a: 2 }, { a: 3 }, { a: 4 }],
+        base,
+        value: { ...base, a: 3 },
+        index: 2,
+      });
+    });
+  });
+
+  describe('backward', () => {
+    it('should go backward', () => {
+      let history = generate(base);
+
+      history = push(history, { ...base, a: 2 });
+      history = push(history, { ...base, a: 3 });
+      history = push(history, { ...base, a: 4 });
+
+      history = backward(history);
+
+      expect(history).toEqual({
+        list: [{ a: 2 }, { a: 3 }, { a: 4 }],
+        base,
+        value: { ...base, a: 3 },
+        index: 2,
+      });
+    });
+
+    it('should not go backward if at the beginning', () => {
+      let history = generate(base);
+
+      history = push(history, { ...base, a: 2 });
+      history = push(history, { ...base, a: 3 });
+      history = push(history, { ...base, a: 4 });
+
+      history.index = 0;
+      history = backward(history);
+
+      expect(history).toEqual({
+        list: [{ a: 2 }, { a: 3 }, { a: 4 }],
+        base,
+        value: base,
+        index: 0,
+      });
+    });
+  });
+
+  describe('forward', () => {
+    it('should go forward', () => {
+      let history = generate(base);
+
+      history = push(history, { ...base, a: 2 });
+      history = push(history, { ...base, a: 3 });
+      history = push(history, { ...base, a: 4 });
+
+      history.index = 0;
+      history = forward(history);
+
+      expect(history).toEqual({
+        list: [{ a: 2 }, { a: 3 }, { a: 4 }],
+        base,
+        value: { ...base, a: 2 },
+        index: 1,
+      });
+    });
+
+    it('should not go forward at the end', () => {
+      let history = generate(base);
+
+      history = push(history, { ...base, a: 2 });
+      history = push(history, { ...base, a: 3 });
+      history = push(history, { ...base, a: 4 });
+      history = forward(history);
+
+      expect(history).toEqual({
+        list: [{ a: 2 }, { a: 3 }, { a: 4 }],
+        base,
+        value: { ...base, a: 4 },
+        index: 3,
+      });
+    });
   });
 });
--- src/lib/history.ts
+++ src/lib/history.ts
@@ -1,40 +1,67 @@
 import { applyPartialDiff, getPartialDiff } from './util';
 
-export type History<T extends Record<string, any>> = [T, ...Partial<T>[]];
-
-export function forward<T extends Record<string, any>>(
-  history: History<T>,
-  value: T
-): History<T> {
-  if (history.length === 0) {
-    return [value];
-  }
-
-  return [...history, getPartialDiff(history.at(-1)!, value)];
+// export type HistoryList<T extends Record<string, any>> = [T, ...Partial<T>[]];
+export interface History<T> {
+  list: Array<Partial<T>>;
+  base: T;
+  value: T;
+  index: number;
 }
 
-/**
- * Discards the last value in the history. If the history is at the beginning, it will return the same history.
- */
+// History is a list of partial diffs, and a base state.
+// It allows us to go back and forth in time, and apply the diffs to the base state.
+// We can push a state to the history, and then go back and forth in time.
+// If we push a new state while we're not at the end of the history, we'll remove the future states.
+
+export function generate<T extends Record<string, any>>(base: T): History<T> {
+  return {
+    list: [],
+    base,
+    value: base,
+    index: 0,
+  };
+}
+
+export function push<T extends Record<string, any>>(
+  history: History<T>,
+  state: T
+): History<T> {
+  const diff = getPartialDiff(history.value, state);
+  const list = [...history.list.slice(0, history.index), diff];
+
+  const index = list.length;
+  const value = state;
+  return { ...history, list, index, value };
+}
+
+export function assemble<T extends Record<string, any>>(
+  history: History<T>
+): T {
+  // use array.reduce to apply the diffs to the base state
+  return history.list
+    .slice(0, history.index)
+    .reduce<T>((state, diff) => applyPartialDiff(state, diff), history.base);
+}
+
+export function toIndex<T extends Record<string, any>>(
+  history: History<T>,
+  index: number
+): History<T> {
+  index = Math.max(0, Math.min(index, history.list.length));
+
+  const value = assemble({ ...history, index });
+  return { ...history, index, value };
+}
+
+export function forward<T extends Record<string, any>>(
+  history: History<T>
+): History<T> {
+  if (history.index === history.list.length) return history;
+  return toIndex(history, history.index + 1);
+}
+
 export function backward<T extends Record<string, any>>(
   history: History<T>
 ): History<T> {
-  if (history.length === 1) return history;
-  // @ts-expect-error: TS doesn't know that the first element is always present
-  return history.slice(0, -1);
-}
-
-/**
- * Rebuilds the state up to the given index.
- * @param index defaults to the last index
- */
-export function rollback<T extends Record<string, any>>(
-  history: History<T>,
-  index?: number
-): T {
-  index = index ?? history.length - 1;
-  // @ts-expect-error: TS doesn't know that the first element is always present
-  return history
-    .slice(0, index + 1)
-    .reduce((acc, diff) => applyPartialDiff(acc, diff), history[0]);
+  return toIndex(history, history.index - 1);
 }
