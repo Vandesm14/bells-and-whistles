9e7d0b9893b0c32a6e7c5f722eb10f028bc25514
Shane Vandegrift <vandesm14@gmail.com>
2022-09-11 23:09:09 -0400

    fix switch CSS

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -23,8 +23,23 @@ export function Switch({ label, top, bottom, value, onChange }: SwitchProps) {
   };
 
   const bottomValue = bottom !== undefined ? bottom.on : value;
+  const bottomColor = bottom?.color || colors.white;
 
+  const PADDING_W = 4;
+  const PADDING_H = 2;
+  const BORDER = 2;
   const SIZE = 50;
+  const LINE_HEIGHT = 20;
+
+  const TEXT_HEIGHT = SIZE / 2 - PADDING_H * 2 + 4;
+
+  const commonText: React.CSSProperties = {
+    display: 'block',
+    width: '100%',
+    height: `${TEXT_HEIGHT + 4}px`,
+    textAlign: 'center',
+    lineHeight: `${LINE_HEIGHT}px`,
+  };
 
   return (
     <Column
@@ -37,34 +52,29 @@ export function Switch({ label, top, bottom, value, onChange }: SwitchProps) {
       {label ? <span>{label.toUpperCase()}</span> : null}
       <Column
         style={{
-          border: '2px solid white',
+          border: '2px solid #f6c29a',
           backgroundColor: 'black',
           width: SIZE,
           height: SIZE,
+          padding: `${PADDING_H}px ${PADDING_W}px`,
         }}
         center
       >
         <b
           style={{
-            height: SIZE / 2 - 2,
             color: top?.color || colors.status.orange,
+            margin: `${BORDER}px 0`, // compensating for the border
+            ...commonText,
           }}
         >
-          {top?.on ? String(top.text || 'fault').toUpperCase() : null}
+          {top?.on ? String(top.text || 'fault').toUpperCase() : ''}
         </b>
-        <hr
-          style={{
-            width: SIZE - 4,
-            height: 2,
-            backgroundColor: 'white',
-            margin: 0,
-          }}
-        />
         <b
           style={{
-            height: SIZE / 2 - 2,
-            // border: '2px solid white',
-            color: bottom?.color || colors.white,
+            color: bottomColor,
+            border: !bottomValue ? `${BORDER}px solid ${bottomColor}` : 'none',
+            margin: bottomValue ? `${BORDER}px 0` : '0', // compensating for the border
+            ...commonText,
           }}
         >
           {!bottomValue ? String(bottom?.text || 'off').toUpperCase() : null}
@@ -76,8 +86,8 @@ export function Switch({ label, top, bottom, value, onChange }: SwitchProps) {
           style={{
             position: 'absolute',
             opacity: 0,
-            width: SIZE,
-            height: SIZE,
+            width: SIZE + PADDING_W * 2 + BORDER * 2,
+            height: SIZE + PADDING_H * 2 + BORDER * 2,
             margin: 0,
           }}
         />
